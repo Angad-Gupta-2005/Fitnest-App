c5db7a13e427b4a52ab9e2c3705ee54d408ec538
Angad Gupta <127626872+Angad-Gupta-2005@users.noreply.github.com>
2024-09-24 09:56:41 +0530

    splash screen part 1 done

--- app/src/main/java/com/angad/fitnestx/onboarding/Splash1Fragment.kt
+++ app/src/main/java/com/angad/fitnestx/onboarding/Splash1Fragment.kt
@@ -1,21 +1,33 @@
 package com.angad.fitnestx.onboarding
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.angad.fitnestx.R
+import com.angad.fitnestx.databinding.FragmentSplash1Binding
 
 
 class Splash1Fragment : Fragment() {
+
+private lateinit var binding: FragmentSplash1Binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash1, container, false)
+        binding = FragmentSplash1Binding.inflate(layoutInflater)
+
+        //        Use of Handler for splash screen
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.action_splash1Fragment_to_splash2Fragment)
+        }, 3000)
+        return  binding.root
     }
 
 }
